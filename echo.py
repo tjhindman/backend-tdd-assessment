2886ffddb984429385bc47620ea10e8a28507e01
TJ Hindman <thind131@gmail.com>
2018-12-11 11:27:19 -0500

    finished tdd

--- echo.py
+++ echo.py
@@ -2,21 +2,59 @@
 # -*- coding: utf-8 -*-
 """An enhanced version of the 'echo' cmd line utility"""
 
-__author__ = "???"
+__author__ = "tjhindman"
 
 
 import sys
+import argparse
+
+
+def upper(text):
+    up = text.upper()
+    return up
+
+
+def lower(text):
+    low = text.lower()
+    return low
+
+
+def title(text):
+    title = text.title()
+    return title
 
 
 def create_parser():
     """Creates and returns an argparse cmd line option parser"""
-    pass
+
+    parser = argparse.ArgumentParser(description="Perform transformation on input text.")
+    parser.add_argument("text", help="text to be manipulated")
+    parser.add_argument("-u", "--upper", help="convert text to uppercase", action="store_true")
+    parser.add_argument("-l", "--lower", help="convert text to lowercase", action="store_true")
+    parser.add_argument("-t", "--title", help="convert text to titlecase", action="store_true")
+
+    return parser
 
 
 def main(args):
     """Implementation of echo"""
-    pass
+
+    arg_check = create_parser()
+
+    if not args:
+        arg_check.print_usage()
+        sys.exit()
+
+    args = arg_check.parse_args(args)
+
+    if args.upper:
+        upper(args.text)
+    if args.lower:
+        lower(args.text)
+    if args.title:
+        title(args.text)
+
 
 
 if __name__ == '__main__':
-    pass
+    main(sys.argv[1:])
