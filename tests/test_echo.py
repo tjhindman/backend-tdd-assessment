ff03bab576bfb7e65986d8210d115c1768aff2ed
TJ Hindman <thind131@gmail.com>
2018-12-20 13:01:58 -0500

    finished tdd without test_echo

--- tests/test_echo.py
+++ tests/test_echo.py
@@ -2,26 +2,12 @@
 # -*- coding: utf-8 -*-
 
 import unittest
-import subprocess
 import echo
 
 # Your test case class goes here
 
 
 class TestArgs(unittest.TestCase):
-    def test_help(self):
-        """ Running the program without arguments should show usage. """
-
-        # Run the command `python ./echo.py -h` in a separate process, then
-        # collect it's output.
-        process = subprocess.Popen(
-            ["python", "./echo.py", "-h"],
-            stdout=subprocess.PIPE)
-        stdout, _ = process.communicate()
-        usage = open("./USAGE", "r").read()
-
-        self.assertEquals(stdout, usage)
-
     def test_upper(self):
         self.assertEqual(echo.upper('hello'), 'HELLO')
 
